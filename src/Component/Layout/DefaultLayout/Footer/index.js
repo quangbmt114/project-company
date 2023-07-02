35e7d3cd5e591b2ac5a2e3c73a8a7743afa8a8e0
quangbmt114 <quangbmt1b@gmail.com>
2023-07-02 20:42:01 +0700

    set layout

---
+++ src/Component/Layout/DefaultLayout/Footer/index.js
@@ -0,0 +1,7 @@
+function Footer() {
+    return ( <div>
+        <h2>Footer</h2>
+    </div> );
+}
+
+export default Footer;
--- src/Component/Layout/DefaultLayout/index.js
+++ src/Component/Layout/DefaultLayout/index.js
@@ -1,7 +1,16 @@
-function DefaultLayout() {
-    return ( <div>
-        
-    </div> );
+import Footer from "./Footer";
+import Header from "./Header";
+
+function DefaultLayout({children}) {
+  return (
+    <div>
+      <Header />
+      <div className="content">
+            {children}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default DefaultLayout;
