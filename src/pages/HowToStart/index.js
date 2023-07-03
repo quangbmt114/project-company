fc792e49dfaac8855c67c2181ff6ee455301d50c
Truong2003 <nguyentruongbmt2003@gmail.com>
2023-07-03 20:21:00 +0700

    update

--- src/pages/HowToStart/index.js
+++ src/pages/HowToStart/index.js
@@ -1,7 +1,109 @@
+import "../HowToStart/style.css"
+import Banner from "../HowToStart/img/banner.png"
+import Banner2 from "../HowToStart/img/banner2.png"
+import Img from "../HowToStart/img/img.png"
+
+import G1 from "../HowToStart/img/g1.png"
+import G2 from "../HowToStart/img/g2.png"
+import G3 from "../HowToStart/img/g3.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faChevronRight
+} from '@fortawesome/free-solid-svg-icons';
+
 function HowToStart() {
-    return ( <>
-    <h1>How to Start Page</h1>
-    </> );
+    return (
+        <>
+            <div className="banner pb-5 bg-cover bg-center d-flex align-items-center justify-content-center">
+                <img src={Banner} className="w-100" alt="banner" />
+                <div className="container position-absolute">
+                    <div className="row">
+                        <div className="text-white text-center">
+                            <h1 className="title mb-3 fw-bold">How to Start</h1>
+                            <p className="content">We specialize in helping you build a team of expert developers.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="text-start">
+                    <div className="col-12 col-md-4" style={{ color: "#16205F" }}>
+                        <h3 className="title fw-bold">How to Get Started with Courtney</h3>
+                        <p style={{fontWeight: "300px"}}> From full-time remote engineering teams to hourly
+                            contractors, work with remote devs as needed
+                        </p>
+                    </div>
+                </div>
+
+                <div className="row py-5">
+                    <div className="col-sm-4">
+                        <div className="d-inline-block">
+                            <div className="d-flex align-items-end">
+                                <img src={G1} className="img-fluid" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="d-inline-block">
+                            <div className="d-flex align-items-end">
+                                <img src={G2} className="img-fluid" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="d-inline-block">
+                            <div className="d-flex align-items-end">
+                                <img src={G3} className="img-fluid" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="text-start">
+                    <div className="col-12 col-md-4" style={{ color: "#16205F" }}>
+                        <h3>Why You Need To Hire Remote Developers</h3>
+                    </div>
+                </div>
+
+                <div className="text-start">
+                    <div className="row py-3" style={{ color: "#16205F" }}>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Talent shortage
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Lack of Expertise
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Quick Kick-Off
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Cost Optimization
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Talen Accessibility
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <FontAwesomeIcon icon={faChevronRight} /> Administrative Hassle
+                        </div>
+                    </div>
+                </div>
+                 
+                <div className="py-5">
+                    <img src={Banner2} className="w-100" alt="" />
+                    <div className="d-flex align-items-center">
+                        <div className="image-container">
+                            <img src={Img} alt="" className="img-fluid w-100" />
+                            <div className="overlay">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+ 
+            </div>
+        </>
+    );
 }
 
 export default HowToStart;
