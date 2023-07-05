72f267b8421ad57783e5b767a109065c931d0d22
ABCTuanAnh <tuananhcfnam@gmail.com>
2023-07-05 16:21:48 +0700

    update

--- src/pages/Technologies/index.js
+++ src/pages/Technologies/index.js
@@ -6,6 +6,38 @@ import E from '../Technologies/img/e-icon.png'
 import HTML from '../Technologies/img/html.png'
 import CSS from '../Technologies/img/css.png'
 import NodeJs_Logo from '../Technologies/img/Node.js_logo.png'
+import JavaLogo from '../Technologies/img/Java_logo 1.png'
+import PythonLogo from '../Technologies/img/PythonLogo.png'
+import PHP from '../Technologies/img/PHP-logo.png'
+import C1 from '../Technologies/img/c++.png'
+import C2 from '../Technologies/img/C++_Logo 1.png'
+import Laravel from '../Technologies/img/laravel-logo.png'
+import Symfony from '../Technologies/img/symfony-logo.png'
+import django from '../Technologies/img/Django-logo.png'
+import expressJs from '../Technologies/img/expressJs.png'
+import Spring from '../Technologies/img/Spring-logo.png'
+import IOS from '../Technologies/img/iOS-Logo.png'
+import reactNative from '../Technologies/img/react-native.png'
+import flutter from '../Technologies/img/flutter-logo.png'
+import kotlin from '../Technologies/img/Kotlin_logo 1.png'
+import lonic from '../Technologies/img/Ionic_Logo.png'
+import Mysql from '../Technologies/img/MySQL-Logo.png'
+import Sqlsever from '../Technologies/img/sql-server-logo.png'
+import postgresql from '../Technologies/img/postgresql-icon.png'
+import mariadb from '../Technologies/img/mariadb-logo.png'
+import mongodbLogo from '../Technologies/img/mongodb_logo.png'
+import redis from '../Technologies/img/Redis-Logo.png'
+import Oracle from '../Technologies/img/Oracle-Logo.png'
+import go from '../Technologies/img/Go_Logo.png'
+import angular from '../Technologies/img/angular-logo.png'
+import androilogo from '../Technologies/img/android_logo.png'
+import studioLogo from '../Technologies/img/studiologo.png'
+import hadoop from '../Technologies/img/hadoop-logo.png'
+import PowerBi from '../Technologies/img/powerBI.png'
+import OpenCv from '../Technologies/img/OpenCV_logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import Img from "../HowToStart/img/img.png";
 
 function Technologies() {
     return (
@@ -105,9 +137,214 @@ function Technologies() {
                             <img src={NodeJs_Logo} alt="" />
                         </button>
                     </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={JavaLogo} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={PythonLogo} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={PHP} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={C1} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={C2} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Laravel} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Symfony} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={django} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={expressJs} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Spring} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={IOS} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={reactNative} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={flutter} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={kotlin} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={lonic} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Mysql} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Sqlsever} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={postgresql} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={mariadb} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={mongodbLogo} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={redis} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={Oracle} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={go} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={angular} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={androilogo} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={studioLogo} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={hadoop} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={PowerBi} alt="" />
+                        </button>
+                    </div>
+                    <div className="col-6 col-md-auto ">
+                        <button type="button" className="btn btn-link">
+                            <img src={OpenCv} alt="" />
+                        </button>
+                    </div>
                 </div>
 
+                
+                
+
             </div>
+            <div className="row custom-group position-relative">
+                    
+                    <div className="col">
+                        <div className="d-flex">
+                            <img src={Img} alt="" className="img-fluid"/>
+                        </div>
+                        <div className="custom-group-overlay">
+                            <div className="custom-group-content">
+                                <h1 className="fw-bold text-start">
+                                    Connect With Your Next Great Hire Today!
+                                </h1>
+                                <form className="py-3" style={{ fontSize: "16px", lineHeight: "24px" }}>
+                                    <div className="row">
+                                        <div className="col-md-6 form-group text-start mb-3">
+                                            <label htmlFor="">Company</label>
+                                            <input className="form-control" placeholder="Enter company" required></input>
+                                        </div>
+                                        <div className="col-md-6 form-group text-start mb-3">
+                                            <label htmlFor="">Your Name</label>
+                                            <input className="form-control" placeholder="Enter your name" required></input>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="col-md-6 form-group text-start mb-3">
+                                            <label htmlFor="">Phone Number</label>
+                                            <input className="form-control" placeholder="Enter phone number" required></input>
+                                        </div>
+                                        <div className="col-md-6 form-group text-start mb-3">
+                                            <label htmlFor="">Email</label>
+                                            <input className="form-control" placeholder="Enter email" required></input>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="col-12 form-group text-start mb-3">
+                                            <label htmlFor="">Project Detail</label>
+                                            <textarea className="form-control" rows={5} placeholder="Enter project detail"></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="form-group form-check py-3">
+                                        <input className="form-check-input" type="checkbox" id="exampleCheck1" />
+                                        <label className="form-check-label text-start" htmlFor="exampleCheck1" style={{ fontSize: "16px", lineHeight: "24px" }}>
+                                            By sending this form I confirm that I have read and accept the
+                                            <span className="fw-bold"> Privacy Policy</span>
+                                        </label>
+                                    </div>
+                                    <div className="d-flex justify-content-start">
+                                        <button className="btn btn-warning" style={{ borderRadius: "8px" }}>
+                                            GET CONSULTATION <FontAwesomeIcon icon={faArrowRight} />
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </> 
         );
 }
