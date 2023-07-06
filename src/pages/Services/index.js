e2f0d99fc4bef21eb698139f0dd28b33417ae15d
DangHH2003 <137130413+DangHH2003@users.noreply.github.com>
2023-07-06 13:58:51 +0700

    update index service

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -6,11 +6,11 @@ import G2 from "../Services/img/G2.png";
 import G3 from "../Services/img/G3.png";
 import G4 from "../Services/img/G4.png";
 
-import { faChevronRight, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 function Services() {
     return (
-    <>
-               <div className="banner-ourservices">
+        <>
+            <div className="banner-ourservices">
                 <div className="banner-ourservices1">
                     <div className="row d-flex justify-content-center align-items-center">
                         <div className="col-md-9">
@@ -25,34 +25,56 @@ function Services() {
                 <div className="header_ourservices">
                     <div className="col-sm-6 text-start">
                         <h1 className="title fw-bold">Hire Permanent and Remote Developers</h1>
-                        </div>
+                    </div>
                     <div className="col-sm-5 text-start">
                         <p>From full-time remote engineering teams to hourly contractors, work with remote devs as needed</p>
                     </div>
                 </div>
 
                 <div class="container py-5">
-                <div class="row">
-                    <div class="col">`
-                        <img src={G1}></img>
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <div className="in4-img"><img src={G1} class="rounded float-start" alt="..."></img>
+                            </div>
+                            <div className="in4-text">
+                                <h1>Dedicated Teams</h1>
+                                <p>Building an extended team with Courtney is just like opening your own remote development center, but without the hassle.</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="in4-img"><img src={G2} class="rounded float-start" alt="..."></img>
+                            </div>
+                            <div className="in4-text">
+                                <h1>Staff Augmentation</h1>
+                                <p>IT Staff Augmentation is a service designed to add extra talent to your team on an on-demand basis. This allows businesses to immediately find the right fit for hard-to-fill or temporary positions, which boosts the scalability and efficiency of project development.</p>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div className="in4-img"><img src={G3} class="rounded float-start" alt="..."></img>
+                            </div>
+                            <div className="in4-text">
+                                <h1>Software Outsourcing</h1>
+                                <p>Today, organizations all over the world deal with software outsourcing companies to have access to experienced software engineers with a variety of tech expertise ranging from user experience design to blockchain consulting.</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="in4-img"><img src={G4} class="rounded float-start" alt="..."></img>
+                            </div>
+                            <div className="in4-text">
+                                <h1>Remote Office</h1>
+                                <p>Courtney is a trustworthy partner that can help you open your own remote development center and grow your business from Sri Lanka and United Kingdom.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col">
-                        <img src={G2}></img>
-                        </div>
-                    <div class="w-100"></div>
-                    <div class="col">
-                        <img src={G3}></img>
-                        </div>
-                    <div class="col">
-                        <img src={G4}></img>
-                       </div>
                 </div>
-            </div>
+
+
 
                 <div className="row custom-group position-relative">
                     <div className="col">
                         <div className="d-flex">
-                            <img src={Img} alt="" className="img-fluid"/>
+                            <img src={Img} alt="" className="img-fluid" />
                         </div>
                         <div className="custom-group-overlay">
                             <div className="custom-group-content">
@@ -106,7 +128,7 @@ function Services() {
                     </div>
                 </div>
             </div>
-    </>
+        </>
 
     );
 }
