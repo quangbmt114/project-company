863df763b6b341c9a28d46d7e88493dceb286e66
Truong2003 <nguyentruongbmt2003@gmail.com>
2023-07-07 13:32:43 +0700

    update header

--- src/Component/Layout/DefaultLayout/Header/index.js
+++ src/Component/Layout/DefaultLayout/Header/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 function Header() {
   return (
-    <div className=" header " lg={12}>
+    <div className="header" lg={12}>
       <Navbar expand="lg" className="bg-body-tertiary ">
         <Container>
           <Navbar.Brand style={{marginRight:"10%",padding:0}}>
@@ -11,11 +11,7 @@ function Header() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll >
               <Nav.Link className="link">
                 <Link to="/howtostart" className="link">How To Start</Link>
               </Nav.Link>
@@ -27,12 +23,14 @@ function Header() {
                 <Link to="/careers" className="link">Careers</Link>
               </Nav.Link>
             </Nav>
-            <Nav className="d-flex">
-                <div className="build-a-team">
-                   <span  className="font-a-team">BUILD A TEAM <img src="arrow_forward.svg"className="svg-a-team" width={20} alt="..." /></span>
+            <Nav>
+                <div className="build-a-team d-flex justify-content-center">
+                  <button className="font-a-team ">
+                    BUILD A TEAM <img src="arrow_forward.svg" className="svg-a-team" width={20} alt="..." />
+                  </button>
                 </div>
                 <Nav.Link className="link">
-                <Link to="/contactus" className="link">Contact US</Link>
+                <Link to="/contactus" className="link">Contact us</Link>
               </Nav.Link>
                 </Nav>
           </Navbar.Collapse>
